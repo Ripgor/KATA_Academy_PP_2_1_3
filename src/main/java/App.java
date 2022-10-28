719dc189ebe644cff186eb9fbf7c3af6cce647da
Calculator <eschigreev@gmail.com>
2022-10-28 10:38:15 +0700

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,22 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
+        // создание context для чтения config-файла
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // Создание бинов
+        // Здесь Возвращается тип Object, поэтому мы самостоятельно кастим до нужного класса
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        // А вот здесь я явно указал класс и кастить не нужно
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("Is bean equal to bean2? " + (bean == bean2));       // true
+        System.out.println("Is cat1 equal to cat2? " + (cat1 == cat2));     // false
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,13 +2,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-@Configuration
+@Configuration      // аннотация для обозначения конфиг-класса
 public class AppConfig {
  
-    @Bean(name="helloworld")
+    @Bean(name="helloworld")        // @Bean "управляет" созданием объектов
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Bean(name="cat")
+    @Scope("prototype")     // Чтобы каждый раз создавался новый объект
+    public Cat getCat() {
+        Cat cat= new Cat();
+        cat.setName("Mur");
+        return cat;
     }
 }
